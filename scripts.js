7df278a34e5ae3ad5e338fd6ca28a248a9316eab
chunktooth <om.saetehn@gmail.com>
2018-05-14 12:55:04 -0600

    Initital commit

---
+++ scripts.js
@@ -0,0 +1,32 @@
+$(document).ready(function() {
+  $('.accordion-list').each(function(index) {
+    $(this)
+    .find('li').first()
+    .find('a').addClass('active')
+    .next().addClass('show').show()
+  });
+
+  $('.accordion-list').on('click', '.tab-anchor', function(event) {
+    event.preventDefault();
+    let tabMenu = $(this).closest('.accordion-list'); 
+
+    if(!$(this).hasClass('active')) {
+      tabMenu
+        .find('.show')
+        .removeClass('show')
+        .hide()
+
+      $(this)
+        .next()
+        .toggleClass('show')
+        .toggle();
+
+      tabMenu
+        .find('.active')
+        .removeClass('active');
+
+      $(this).addClass('active');
+    }
+  });
+});
+
