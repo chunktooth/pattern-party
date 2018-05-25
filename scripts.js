8a62641650f9205dcf1e7dd81ad0baa8b461dba7
chunktooth <om.saetehn@gmail.com>
2018-05-25 16:42:19 -0600

    jQuery toggle hidden menu

--- scripts.js
+++ scripts.js
@@ -1,23 +1,5 @@
-let activeTab = 1;
-
- if($('.tab-list:last-child').hasClass('active'))
-    activeTab=1;
-  else
-    activeTab++;
- 
-$('.tab-list:last-child('+activeTab+') > a').trigger('click');
-
-$('.tab-content').hide();
-
-$('.tab-list:first')
-  .addClass('active')
-  .show();
-
-$('.tab-content:first')
-  .show();
-
-$('.tab-list').on('click', function() {
-  $('.tab-list').removeClass('active');
-  $(this).toggleClass('active');
-  $('.tab-content').hide();
+$('.logo').on('click', function() {
+  $('.nav-menu, .search-bar')
+  .toggleClass('hidden-menu');
+  // $('.search-bar').toggleClass('hidden-menu');
 });
