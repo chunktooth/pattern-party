cc62779acaa7460015b0dfc62187ac864bf5e717
chunktooth <om.saetehn@gmail.com>
2018-05-26 12:23:47 -0600

    toggling tabs on desktop view

--- scripts.js
+++ scripts.js
@@ -1,22 +1,44 @@
-// $('.tab-content').hide();
-// $('.tab-content:first').show();
+$('.tab-list').on('click', toggleTabs);
+$('.menu-header').on('click', toggleNav);
+$('.tab-content').hide();
+$('.tab-content:first').show();
 
-// $('.tab-list').on('click', function() {
-//   $('.tab-list').removeClass('active');
-//   $(this).addClass('active');
+function toggleTabs() {
+  let tabList = $('.tab-list');
+  let tabId = $(this).attr('id');
 
-//   let tabId = $(this).find('a').attr('href');
+  tabList.each(function(i, li) {
+    if($(li).attr('id') === tabId) {
+      tabList.removeClass('active');
+      $(this).addClass('active');
+      displayContent(tabId);
+    }
+  });
+}
+
+function displayContent(tabId) {
+  let tabContent = $('.tab-content');
+
+  tabContent.each(function(i, article) {
+    if($(article).attr('id') === tabId) {
+      $(article).show();
+    } else {
+      $(article).hide()
+    }
+  });
+}
+
+function expandMenu(tabList) {
+  tabList.each(function(i, li) {
+    if($(li).hasClass('active')) {
+      $(this).find('.expand').text('-');
+    } else {
+      $(this).find('.expand').text('+');
+    }
+  });
+};
   
-//   console.log(tabId)
-
-//   $('.tab-list').each(function(i, tabList) {
-//     if(tabList)
-//   });
-
-// });
-
-
-$('.menu-header').on('click', function() {
+function toggleNav() {
   $('.nav-menu, .search-bar')
   .toggleClass('hidden-menu');
-});
+};
