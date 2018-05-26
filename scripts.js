1885d6c63bb5e0edd59f6acb9c49150dcf2899f5
chunktooth <om.saetehn@gmail.com>
2018-05-26 09:09:56 -0600

    fine-tuned responsive title headers

--- scripts.js
+++ scripts.js
@@ -1,3 +1,21 @@
+// $('.tab-content').hide();
+// $('.tab-content:first').show();
+
+// $('.tab-list').on('click', function() {
+//   $('.tab-list').removeClass('active');
+//   $(this).addClass('active');
+
+//   let tabId = $(this).find('a').attr('href');
+  
+//   console.log(tabId)
+
+//   $('.tab-list').each(function(i, tabList) {
+//     if(tabList)
+//   });
+
+// });
+
+
 $('.menu-header').on('click', function() {
   $('.nav-menu, .search-bar')
   .toggleClass('hidden-menu');
